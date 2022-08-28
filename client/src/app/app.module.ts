02052b76ab6c77c4fbc4465a18ddfb6e9617ff9f
mrfishchev <mrfishchev@seniorvlogger.com>
2022-08-28 11:42:14 +0300

    (feat): FE and BE are connected

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoursesComponent } from './courses/courses.component';
+
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, CoursesComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
